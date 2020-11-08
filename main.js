7892f2335246efed831b339cc5f075cd95dcbe7c
Hannah Kim <hannah@Hannahs-MacBook-Pro.local>
2020-11-07 21:54:31 -0500

    new website layout

--- main.js
+++ main.js
@@ -1,24 +1,10 @@
+
 $(document).ready(function (){
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if(scroll > 100){
-            $("nav").addClass("bar_scroll");  
-            $("nav").removeClass("bar_og"); 
-        }
-        else{
-            $("nav").removeClass("bar_scroll");
-            $("nav").addClass("bar_og");
-        }
-    })
-    $(document).on('click', 'a[href^="#"]', function (event) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top-75
-        }, 500);
-    })
-    $('input[type="radio"]').click(function() { 
-        var inputValue = $(this).attr("value");
-        var inputClass = $(this).attr("id");
+
+    $('.radio_butt').click(function() { 
+        var inputted = $(this).find('input');
+        var inputValue = inputted.attr("value");
+        var inputClass = inputted.attr("id");
         var targetBox = $("." + inputValue); 
         var targetClass = $("." + inputClass); 
         $(".selected").not(targetBox).fadeOut(300); 
